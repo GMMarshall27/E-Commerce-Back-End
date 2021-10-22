4dcb0d7db850e20ca75db7ca326f48063f92de95
GMMarshall <gmmarshall27@gmail.com>
2021-10-22 17:08:27 -0400

    finished tag routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -49,14 +49,51 @@ router.get('/:id', (req, res) => {
 
 router.post('/', (req, res) => {
   // create a new tag
+  Tag.create({
+    tag_name: req.body.tag_name
+  }).then(tagData => res.json(tagData))
+  .catch (err => {
+    res.status(500).json(err) 
+    
+  });
 });
 
 router.put('/:id', (req, res) => {
   // update a tag's name by its `id` value
+  Tag.update({
+    where: {
+      id: req.params.id
+    }
+  }).then(tagData => {
+    if(!tagData) {
+      res.status(404).json({message: 'No tag was found'});
+      return;
+    }
+    res.json(tagData);
+  })
+  .catch (err => {
+    res.status(500).json(err) 
+    
+  });
 });
 
 router.delete('/:id', (req, res) => {
   // delete on tag by its `id` value
+  Tag.destroy({
+    where: {
+      id: req.params.id,
+    },
+  }).then(tagData => {
+    if(!tagData) {
+      res.status(404).json({message: 'No tag was found'});
+      return;
+    }
+    res.json(tagData);
+  })
+  .catch (err => {
+    res.status(500).json(err) 
+    
+  });
 });
 
 module.exports = router;
